227fc202d4773892c9b4fd863c86229f7ac7b42f
Gabriel Wilson <gabrielvincentw@gmail.com>
2022-08-08 22:12:38 -0400

    Added event callbacks for window events and keyboard events

--- Strife/src/Platform/Windows/WindowsWindow.cpp
+++ Strife/src/Platform/Windows/WindowsWindow.cpp
@@ -1,5 +1,8 @@
 #include "stpch.h"
 #include "WindowsWindow.h"
+#include "Strife/Events/ApplicationEvent.h"
+#include "Strife/Events/KeyEvent.h"
+#include "Strife/Events/MouseEvent.h"
 
 namespace Strife {
 
@@ -42,6 +45,53 @@ namespace Strife {
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				WindowResizeEvent event(width, height);
+				data.Width = width;
+				data.Height = height;
+				data.EventCallback(event); // Set width and height first just in case anything uses them to access the updated size
+			});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				WindowCloseEvent event;
+				data.EventCallback(event);
+			});
+
+		glfwSetWindowFocusCallback(m_Window, [](GLFWwindow* window, int focused)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				WindowFocusEvent event;
+				data.EventCallback(event);
+			});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+			{
+				WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+				switch (action)
+				{
+				case GLFW_PRESS:
+					KeyPressedEvent event(key, 0);
+					data.EventCallback(event);
+					break;
+				case GLFW_REPEAT:
+					KeyPressedEvent event(key, 1);
+					data.EventCallback(event);
+					break;
+				case GLFW_RELEASE:
+					KeyReleasedEvent event(key);
+					data.EventCallback(event);
+					break;
+				}
+			});
 	}
 
 	void WindowsWindow::Shutdown()
--- Strife/src/Strife/Application.cpp
+++ Strife/src/Strife/Application.cpp
@@ -5,9 +5,12 @@
 
 namespace Strife {
 
+#define BIND_EVENT_FN(x) std::bind(x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(&Application::OnEvent));
 	}
 
 	Application::~Application()
@@ -21,6 +24,11 @@ namespace Strife {
 		{
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		ST_CORE_INFO("{0}", e);
 	}
 
 }
